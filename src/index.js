b057511f90eb6ca63a4090443202544b5ae5aaf2
malikH88 <malik.hyamine@gmail.com>
2020-06-08 16:25:54 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { createStore } from 'redux';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import lightReducer from './lightReducer';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(lightReducer);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ src/lightReducer.js
@@ -0,0 +1,9 @@
+// lightReducer.js
+const lightReducer = (state = "on", action) => {
+    if (action.type === "SWITCH") {
+      return state === "on" ? "off" : "on";
+    }
+    return state;
+  };
+  
+export default lightReducer;
